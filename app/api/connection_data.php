7acf93ad3e70b4b484109e635baed114eb11df89
Przemek Kudła <przemek.kudla@nurtureagency.com>
2018-01-04 23:33:46 +0100

    connections credensials update

--- app/api/connection_data.php
+++ app/api/connection_data.php
@@ -1,9 +1,8 @@
 <?php
 $server_address = 'localhost';
-//$user_login = 'przem94_AI';
-//$user_password = 'lomtjjzAI';
-//$db_name = 'przem94_AI';
-$user_password = '';
-$db_name = 'findyourjob';
-$user_login = 'root';
-//test
+$user_login = 'przem94_AI';
+$user_password = 'lomtjjzAI';
+$db_name = 'przem94_AI';
+//$user_password = '';
+//$db_name = 'findyourjob';
+//$user_login = 'root';
